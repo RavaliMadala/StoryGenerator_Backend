28f03e11a3d45b7b11faecc82c75475d264c7f99
MT549 <monica.turaka@eagles.oc.edu>
2024-06-11 15:50:05 -0500

    Code for Sessions

--- src/config/config.js
+++ src/config/config.js
@@ -3,12 +3,12 @@ const path = require('path')
 module.exports = {
   port: process.env.PORT || 8081,
   db: {
-    database: process.env.DB_NAME || 'Storygenerator',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASS || '',
+    database: process.env.DB_NAME || 'travelproject1',
+    user: process.env.DB_USER || 'travelproject1',
+    password: process.env.DB_PASS || 'travelproject1',
     options: {
       dialect: process.env.DIALECT || 'mysql',
-      host: process.env.HOST || 'localhost'
+      host: process.env.HOST || 'db4free.net'
     }
   }
 }
