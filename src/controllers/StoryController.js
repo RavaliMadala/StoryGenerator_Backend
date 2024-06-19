f1f7a55b1593bf8e1f6566394df021b82b408db9
MT549 <monica.turaka@eagles.oc.edu>
2024-06-19 17:45:02 -0500

    Deleting story

--- src/controllers/StoryController.js
+++ src/controllers/StoryController.js
@@ -56,5 +56,161 @@ module.exports = {
         error: 'Some issue occured while saving story.'
       })
     }
+  },
+  async getUserStories (req, res) {
+    try {
+      console.log(req.params.id)
+      var returnAry = []
+      var storyCount = 0;
+      await new Promise( async (resolve) => {
+
+        var story = await Story.findAll({
+          attributes: ['id', 'storyTitle', 'userID'],
+          where: {
+              userID: req.params.id
+          },
+          order: [
+            ['id', 'DESC'],
+          ]
+        })
+        var storyReturnFormat = {}
+        var storyVersionReturnFormat = []
+        console.log(story)
+        if(story != null && story.length > 0){
+          console.log("Stories found.")
+          story.forEach(async element => {
+            console.log(storyCount)
+            console.log(story.length)
+            console.log(element.dataValues)
+  
+            // clear vlaues on each loop
+            storyReturnFormat = {}
+            storyVersionReturnFormat = []
+  
+            var storyversions = await StoryVersionControl.findAll({
+              attributes: ['storyTitle', 'storyId', 'storyVersion', 'storyPrompt', 'StoryResponse', 'characterName', 'characterRole', 'setting', 'country', 'language', 'genre', 'wordCount', 'id'],
+              where: {
+                storyId: element.dataValues.id
+              },
+              order: [
+                ['storyVersion', 'ASC'],
+              ]
+            })
+            if(storyversions != null && storyversions.length > 0){
+              storyVersionReturnFormat = []
+              storyversions.forEach(element1 => {
+                element1.dataValues.StoryResponse = element1.dataValues.StoryResponse.toString('utf8')
+                storyVersionReturnFormat.push(element1.dataValues)
+              });
+              storyReturnFormat = {
+                id:element.dataValues.id,
+                storyTitle:element.dataValues.storyTitle,
+                userID:element.dataValues.userID,
+                versions: storyVersionReturnFormat
+              }
+  
+              returnAry.push(storyReturnFormat)
+              console.log(returnAry[0].id)
+              console.log("push.")
+              //res.send({response: returnAry})
+              storyCount++
+            }
+            if(storyCount == story.length){
+            console.log(story.length)
+            resolve(returnAry)
+            }
+          })
+        }
+        else{
+          res.send({status:false, response: "No Stories found"})
+        }
+      }).then((returnAry)=>{
+        res.send({status:true, response: returnAry})
+      })
+    } catch (err) {
+      console.log(err)
+      res.status(400).send({
+        error: 'Some issue occured while saving story.'
+      })
+    }
+  },
+  async addStoryVersion (req,res) {
+    var storyversions = await StoryVersionControl.findOne({
+      attributes: ['storyVersion'],
+      where: {
+        storyId: req.body.storyId
+      },
+      order: [
+        ['storyVersion', 'DESC'],
+      ]
+    })
+    if(storyversions != null){
+      console.log(storyversions.dataValues.storyVersion)
+
+      console.log("Story version Create.")
+        const storyVersion = await StoryVersionControl.create({
+          storyTitle: req.body.title,
+          storyId: req.body.storyId,
+          storyVersion: storyversions.dataValues.storyVersion + 1,
+          storyPrompt: req.body.storyPrompt,
+          StoryResponse: req.body.StoryResponse,
+          characterName: req.body.characterName,
+          characterRole: req.body.characterRole,
+          setting: req.body.setting,
+          country: req.body.country,
+          language: req.body.language,
+          genre: req.body.genre,
+          wordCount: req.body.wordCount,
+        })
+        if(storyVersion){
+          console.log("Story Version control Create.")
+          res.send(storyVersion.toJSON())
+        }
+    }
+  },
+  async deleteStoryVersion (req, res) {
+    console.log("\n\n\n\n\n")
+    try {
+      var storyOId;
+      const storyverDetails = await StoryVersionControl.findOne({
+        where: { id: req.params.id }
+      })
+      console.log("storyverDetails" + storyverDetails)
+      
+      const storyVersionControl = await StoryVersionControl.destroy({
+        where: { id: req.params.id}
+      })
+      if(storyverDetails != null){
+        console.log("find Story")
+
+        storyOId = storyverDetails.dataValues.StoryId
+        const storyvers = await StoryVersionControl.findAll({
+          where: {storyId: storyOId}
+        })
+            console.log("1: "+storyvers)
+            if(storyvers == null || (storyvers != null && storyvers.length < 1)){
+              console.log(storyvers)
+              const Storyd = await Story.destroy({
+                where: { id: storyOId}
+              })
+              if(Storyd != null){
+                console.log("Deleted Story")
+                res.send({status: "Success", deletedUser: storyVersionControl})
+              }
+              else{
+                console.log("Deleted Story")
+                res.send({status: "Success", deletedUser: storyVersionControl})
+              }
+              
+            }else{
+              console.log("Deleted Story")
+              res.send({status: "Success", deletedUser: storyVersionControl})
+            }
+        }
+    } catch (err) {
+      res.status(500).send({
+        error: 'An error has occured trying to delete User: ' + err
+      })
+    }
   }
 }
